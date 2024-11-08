ab81aa729a0a45827884a86ca001abd96995c897
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-11-09 04:18:04 +0600

    Refactor route order and import statements in Routes.jsx

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,72 +1,72 @@
-import { createBrowserRouter } from "react-router-dom";
-import AdminLayout from "../Pages/Layout/AdminLayout/AdminLayout";
-import AdminDashboard from "../Pages/Layout/AdminLayout/AdminDashboard/AdminDashboard";
-import AllStudents from "../Pages/Layout/AdminLayout/AllStudents/AllStudents";
-import Register from "../Pages/Home/Auth/Register";
-import UserVarification from "../Pages/Home/UserVarification/UserVarification";
-import CourceMeterial from "../Pages/Home/CourceMeterial/CourceMeterial";
-import NotFound from "../Pages/Shared/NotFound";
-import ExamDate from "../Pages/Home/ExamDate/ExamDate";
-import AllUsers from "../Pages/Home/AllUsers/AllUsers";
-import ExamQuestion from "../Pages/Home/ExamQus/ExamQuestion";
-import ExamResult from "../Pages/Home/ExamResult/ExamResult";
-import VerifyCertificed from "../Pages/Home/VerifyCertificed/VerifyCertificed";
+import { createBrowserRouter } from 'react-router-dom';
+import AllUsers from '../Pages/Home/AllUsers/AllUsers';
+import Register from '../Pages/Home/Auth/Register';
+import CourceMeterial from '../Pages/Home/CourceMeterial/CourceMeterial';
+import ExamDate from '../Pages/Home/ExamDate/ExamDate';
+import ExamQuestion from '../Pages/Home/ExamQus/ExamQuestion';
+import ExamResult from '../Pages/Home/ExamResult/ExamResult';
+import UserVarification from '../Pages/Home/UserVarification/UserVarification';
+import VerifyCertificed from '../Pages/Home/VerifyCertificed/VerifyCertificed';
+import AdminDashboard from '../Pages/Layout/AdminLayout/AdminDashboard/AdminDashboard';
+import AdminLayout from '../Pages/Layout/AdminLayout/AdminLayout';
+import AllStudents from '../Pages/Layout/AdminLayout/AllStudents/AllStudents';
+import NotFound from '../Pages/Shared/NotFound';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <AdminLayout />,
-        children: [
-            {
-                path: "/admin-dashboard",
-                element: <AdminDashboard />,
-            },
-            {
-                path: "/all-students",
-                element: <AllStudents />,
-            },
-            {
-                path: "/users",
-                element: <AllUsers />,
-            },
-            {
-                path: "/register",
-                element: <Register />,
-            },
-            {
-                path: "/verification",
-                element: <UserVarification />,
-            },
-            {
-                path: "/course-material",
-                element: <CourceMeterial />,
-            },
-            {
-                path: "/exam-date",
-                element: <ExamDate />,
-            },
-            {
-                path: "/exam-question",
-                element: <ExamQuestion />,
-            },
-            {
-                path: "/exam-result",
-                element: <ExamResult />,
-            },
-            {
-                path: "/verify-certificate",
-                element: <VerifyCertificed />,
-            },
-            {
-                path: "*",
-                element: <NotFound />,
-            }
-        ],
-    },
-    {
-        path: "*",
+  {
+    path: '/',
+    element: <AdminLayout />,
+    children: [
+      {
+        path: '/admin-dashboard',
+        element: <AdminDashboard />,
+      },
+      {
+        path: '/all-students',
+        element: <AllStudents />,
+      },
+      {
+        path: '/users',
+        element: <AllUsers />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+      {
+        path: '/verification',
+        element: <UserVarification />,
+      },
+      {
+        path: '/course-material',
+        element: <CourceMeterial />,
+      },
+      {
+        path: '/exam-date',
+        element: <ExamDate />,
+      },
+      {
+        path: '/exam-question',
+        element: <ExamQuestion />,
+      },
+      {
+        path: '/exam-result',
+        element: <ExamResult />,
+      },
+      {
+        path: '/verify-certificate',
+        element: <VerifyCertificed />,
+      },
+      {
+        path: '*',
         element: <NotFound />,
-    }
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
 ]);
 
 export default router;
